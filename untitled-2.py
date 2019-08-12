0f1fc48dc14d2faccc289c33a4796a89495fdf5d
mdawgie <54043444+mdawgie@users.noreply.github.com>
2019-08-12 15:57:29 -0600

    Add files via upload

---
+++ untitled-2.py
@@ -0,0 +1,13 @@
+
+
+def main():
+  
+  name = input("Enter a name")
+  place = input("Enter a location")
+  friend = input("Enter a name")
+  car = input("Enter a vehicle")
+  print("One day",name,"went to the",place,"of there friend",friend,".", name, "got there by riding there",car,".","Once,",name,"got to the",place,"of",friend,"they both said hi and then gave each other hugs. The End")
+    
+  
+if __name__ == "__main__":
+    main()
